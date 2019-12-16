eb64027e14e106bbbb20f2aa77bb7a380793aa48
James Spurgeon <james.spurgeon@paramet.co.uk>
2019-12-16 13:30:59 +0000

    index page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,17 +3,17 @@ import PropTypes from "prop-types";
 import { Link, graphql } from "gatsby";
 
 import Layout from "../components/Layout";
-import Features from "../components/Features";
-import BlogRoll from "../components/BlogRoll";
+import WorkRoll from "../components/WorkRoll";
 
 export const IndexPageTemplate = ({
   image,
   title,
-  heading,
   subheading,
+  contactBox,
   mainpitch,
-  description,
-  intro
+  services,
+  latestWork,
+  testimonials
 }) => (
   <div>
     <div
@@ -26,104 +26,52 @@ export const IndexPageTemplate = ({
         backgroundAttachment: `fixed`
       }}
     >
-      <div
-        style={{
-          display: "flex",
-          height: "150px",
-          lineHeight: "1",
-          justifyContent: "space-around",
-          alignItems: "left",
-          flexDirection: "column"
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-            backgroundColor: "rgb(255, 68, 0)",
-            color: "white",
-            lineHeight: "1",
-            padding: "0.25em"
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-            backgroundColor: "rgb(255, 68, 0)",
-            color: "white",
-            lineHeight: "1",
-            padding: "0.25em"
-          }}
-        >
-          {subheading}
-        </h3>
-      </div>
+      <h1>{title}</h1>
+      <h2>{subheading}</h2>
+      <h4>{contactBox}</h4>
     </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <div id="mainpitch">
+      <h1 className="title">{mainpitch.title}</h1>
+      <h3 className="subtitle">{mainpitch.description}</h3>
+      <button type="button">Find out more about us</button>
+    </div>
+    <div id="services">
+      <h1 className="title">{services.heading}</h1>
+      <h3 className="subtitle">{services.description}</h3>
+      <ul>
+        {services.servicesList &&
+          services.servicesList.map((service, idx) => (
+            <li key={idx + `-serivce`}>{service.title}</li>
+          ))}
+      </ul>
+    </div>
+    <div id="latest-work">
+      <h1 className="title">{latestWork.title}</h1>
+      <h3 className="subtitle">{latestWork.description}</h3>
+      <WorkRoll></WorkRoll>
+    </div>
+    <div id="testimonials">
+      <h1 className="title">{testimonials.heading}</h1>
+      <h3 className="subtitle">{testimonials.description}</h3>
+      <ul>
+        {testimonials.testimonialsList &&
+          testimonials.testimonialsList.map((testimonial, idx) => (
+            <li key={idx + `-testimonial`}>{testimonial.title}</li>
+          ))}
+      </ul>
+    </div>
   </div>
 );
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
-  heading: PropTypes.string,
   subheading: PropTypes.string,
+  contactBox: PropTypes.string,
   mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array
-  })
+  services: PropTypes.object,
+  testimonials: PropTypes.object,
+  latestWork: PropTypes.object
 };
 
 const IndexPage = ({ data }) => {
@@ -134,11 +82,12 @@ const IndexPage = ({ data }) => {
       <IndexPageTemplate
         image={frontmatter.image}
         title={frontmatter.title}
-        heading={frontmatter.heading}
         subheading={frontmatter.subheading}
+        contactBox={frontmatter.contactBox}
         mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
+        services={frontmatter.services}
+        testimonials={frontmatter.testimonials}
+        latestWork={frontmatter.latestWork}
       />
     </Layout>
   );
@@ -166,15 +115,16 @@ export const pageQuery = graphql`
             }
           }
         }
-        heading
         subheading
+        contactBox
         mainpitch {
           title
           description
         }
-        description
-        intro {
-          blurbs {
+        services {
+          title
+          description
+          servicesList {
             image {
               childImageSharp {
                 fluid(maxWidth: 240, quality: 64) {
@@ -182,9 +132,27 @@ export const pageQuery = graphql`
                 }
               }
             }
-            text
+            heading
+            description
           }
-          heading
+        }
+        testimonials {
+          title
+          description
+          testimonialsList {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 240, quality: 64) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            heading
+            description
+          }
+        }
+        latestWork {
+          title
           description
         }
       }
